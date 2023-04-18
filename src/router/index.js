a5f1e5d0db106912319ea10054534fc5e5e88443
林子佑 <qqq23939889@gmail.com>
2023-04-19 02:49:53 +0800

    修復轉跳路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import LogView from '../views/LogView.vue'
 
 const router = createRouter({
-    history: createWebHistory('/'),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
